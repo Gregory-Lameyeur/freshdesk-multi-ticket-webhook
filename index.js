e2786ddb8b974f74148ed193d7abf98c0d4d5efc
unknown <lamegregory@gmail.com>
2025-07-10 12:36:15 +0100

    update

--- index.js
+++ index.js
@@ -44,6 +44,5 @@ app.post("/webhook", async (req, res) => {
   }
 });
 
-app.listen(3000, () =>
-  console.log("Webhook server running on port 3000", API_KEY)
-);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Webhook server running on port ${PORT}`));
